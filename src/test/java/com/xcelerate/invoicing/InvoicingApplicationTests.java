3b9570849ce1c37d587cb996323c09cbf3234296
Rana Rashid <rana-rashid@vircosolutions.com>
2025-05-09 23:48:16 +0500

    Initial commit

---
+++ src/test/java/com/xcelerate/invoicing/InvoicingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xcelerate.invoicing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoicingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
